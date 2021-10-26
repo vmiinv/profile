ca002308f8a9912cb5952277a102430b87ceee87
vmiinv <73807382+vmiinv@users.noreply.github.com>
2021-10-26 17:22:35 +0600

    profile

--- src/main/java/com/example/storeprofileservice/config/FlywayConfiguration.java
+++ src/main/java/com/example/storeprofileservice/config/FlywayConfiguration.java
@@ -1,17 +1,48 @@
 package com.example.storeprofileservice.config;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @Configuration
+@EnableSwagger2
 public class FlywayConfiguration {
 
     @Autowired
     public FlywayConfiguration(DataSource dataSource) {
         Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
+
+
+    @Bean
+    public Docket swaggerConfiguration() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                " Product Catalog API",
+                "Product Catalog APIs",
+                "1.0",
+                "It's Free",
+                new springfox.documentation.service.Contact("Amina", "", ""),
+                "License",
+                "",
+                Collections.emptyList());
     }
 }
