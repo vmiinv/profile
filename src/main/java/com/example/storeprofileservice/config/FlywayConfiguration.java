8a18dd8a755cd03c50d00543f92a55be5a2812b7
L-Law-Liet <a.kadirov.17.06@gmail.com>
2021-11-19 20:35:11 +0500

    fix bugs

--- src/main/java/com/example/storeprofileservice/config/FlywayConfiguration.java
+++ src/main/java/com/example/storeprofileservice/config/FlywayConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.storeprofileservice.config;
 
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -10,6 +12,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.sql.DataSource;
 import java.util.Collections;
 
 
